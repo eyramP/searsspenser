6845d1d02681cb9d79b5b2034e57734047654dc3
Esquire <ackon.k.richard@gmail.com>
2017-07-08 19:22:59 +0000

    folder structure and index page commit

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Searsspenser</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <!-- other css Files-->
+        <link rel="stylesheet" type="text/css" href="css/flat-ui.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row" id="header">
+                <div id="contact-and-social-media" class="row">
+                    <div class="col-md-6 text-center">
+                        <span style="padding-right: 20px;"><i class="fa fa-phone" aria-hidden="true"></i> + 233 00000 0000</span>
+                        <span><i class="fa fa-envelope" aria-hidden="true"></i> info@searspenser.com</span>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <span><i class="fa fa-facebook"></i></span>
+                        <span><i class="fa fa-twitter"></i></span>
+                        <span><i class="fa fa-instagram"></i></span>
+                    </div>
+                </div>
+
+                <div id="menu" class="row">
+                    <nav class="navbar" role="navigation">
+                        <div class="container-fluid">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <div>
+                                    <a class="navbar-brand" href="#" style="color: white;">
+                                        <img src="images/logo.png" alt="Searsspenser" class="img-responsive" width="130px;">
+                                    </a>   
+                                </div>
+                            </div>
+                    
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse navbar-ex1-collapse" style="padding-top: 1%;">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li><a href="#">Home</a></li>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <b class="caret"></b></a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Board of Directors</a></li>
+                                            <li><a href="#">Managing Partners</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Products</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div><!-- /.navbar-collapse -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+
+            <div id="slider" class="row">
+                <div id="carousel-id" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-id" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-id" data-slide-to="1" class=""></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="">
+                            <div class="container">
+                                <div class="carousel-caption">
+                                    <h1>Example headline.</h1>
+                                    <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="">
+                            <div class="container">
+                                <div class="carousel-caption">
+                                    <h1>Another example headline.</h1>
+                                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>         
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                    <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                </div>
+            </div>
+
+            <div id="about-us" class="row text-center" style="padding: 0 10%;">
+                <h3>What we do</h3>
+                <hr style="border: 1px solid #FAC11C; width: 20%;">
+                <p style="text-align: justify;">
+                    GMC Global is a registered limited liability and fully licensed company. We are one of Ghana´s foremost small-scale gold exploration, mining, processing and marketing companies.
+                    Our company own two mining concessions in a rich and high quality gold geological formation in the country. Headquartered in Tema and with operations in major gold mining belt in Ashanti region. Some of the main gold mining companies operating in this area are AngloGold Ashanti, GoldFields, Kinross Gold, and Newmont.
+                    GMC Global is also licensed by PMMC Ghana. Our Gold is refined in Ghana into 24 Carats bars fine purity and traded locally and International to our clients.
+                </p>
+            </div>
+
+            <div id="products-services" class="row text-center">
+                <h3>Products & Services</h3>
+                <hr style="border: 1px solid #FAC11C; width: 20%;">
+            </div>
+
+            <div id="endorsements" class="row text-center">
+                <h3>What Our Customers Say</h3>
+                <hr style="border: 1px solid #FAC11C; width: 20%;">
+                
+            </div>
+
+
+            <div id="footer" class="row">
+                    <h4 style="color: #FAC11C; margin-left: 3%;">SEARSSPENSER</h4>
+                <div class="row" id="footer-main">
+                    <div class="col-md-offset-1 col-md-3 ">
+                        <ul>
+                        <header>PAGE LINKS</header>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Products</li>
+                            <li>Services</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul>
+                        <header>CONTATC DETAILS: </header>
+                            <li>+233 000 0000</li>
+                            <li>info@searsspenser.com</li>
+                            <li>contac@searsspenser.com</li>
+                            <li>facebook.com/searspenser</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ADDRESS>
+                        <header>VISIT US: </header>
+                            Tema, Comm 20,<br>
+                            Hse No. 22,<br>
+                            Acrra - Ghana<br>
+                        </ADDRESS>
+                    </div>
+                </div>
+
+                <div class="row" style="color: lightgray; padding: 0 30px; border-top: 1px solid gray;">
+                    <div class="col-md-6 text-center">
+                        <span style="padding-right: 20px;">Copyright&copy;2017, Searsspenser</span>
+                    </div>
+                    <div class="col-md-6 text-center" id="soc-div">
+                        <span><i class="fa fa-facebook"></i></span>
+                        <span><i class="fa fa-twitter"></i></span>
+                        <span><i class="fa fa-instagram"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/flat-ui.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </body>
+</html>
